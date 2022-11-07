8deb79e49de3aa7efcb7ad734797e924d4421845
Dan Allman <dallman@appliedres.com>
2022-11-06 23:47:00 -0500

    cv storyboard

--- portfolio/src/components/StoryBoardOpenCV.vue
+++ portfolio/src/components/StoryBoardOpenCV.vue
@@ -1,68 +1,63 @@
 <template>
-  <q-list
-    class="q-pb-xl q-my-md"
-    style="max-width: 900px"
-    :bordered="false"
-    :separator="false"
-    :dark="false"
-    :padding="false"
-  >
+  <q-list class="q-pb-xl q-my-md column items-center" style="max-width: 900px" :bordered="false" :separator="false"
+    :dark="false" :padding="false">
     <q-item style="max-width: 850px">
       <q-item-section>
-        <q-item-label
-          :class="{
-            'text-h3': wide,
-            'text-h4': !wide,
-            'text-weight-light text-center': true,
-          }"
-        >
-          A brief, sibling rivalry-fueled forray into Machine Learning using
-          TensorFlow
+        <q-item-label :class="{
+          'text-h3': wide,
+          'text-h4': !wide,
+          'text-weight-light text-center': true,
+        }">
+          <span>
+            OpenCV.js or: How I Learned to Stop Worrying and Love Emscripten
+          </span>
         </q-item-label>
       </q-item-section>
     </q-item>
     <q-separator class="full-width text-primary q-my-md" size="2px" />
-    <StoryPoint :wide="wide" icon="rocket_launch" :left="true">
-      <template #label> It all started with a fun, family game... </template>
+    <StoryPoint :wide="wide" icon="school" :left="true">
+      <template #label> I wanted to recreate my Master's Thesis </template>
       <template #content>
-        Many years ago, 12 to be exact, my dad started putting together a weekly
-        NFL pick 'em for the whole family to complete. Since then, we have
-        completed the Football Pool, as we call it, every single week of the NFL
-        regular season.
+        I was no stranger to computer vision by time I wrote my
+        <a href="https://drive.google.com/file/d/1iKdqP7BXwRY0l_3-FxBx6pX7etZya3wt/view?usp=sharing"
+          target="_blank">thesis</a>,
+        especially with a library like OpenCV. I had used it extensively in both my undergradutae and graduate
+        education,
+        but always within the context of a C++ project.
       </template>
     </StoryPoint>
-    <StoryPoint :wide="wide" icon="assignment">
-      <template #label> How does it work? </template>
+    <StoryPoint :wide="wide" icon="public">
+      <template #label> Why not bring it to the web? </template>
       <template #content>
-        The game is simple, every week, you have to pick a winner for each NFL
-        game based on the money line (pick who wins each game, regardless of the
-        spread). Whoever has the most picks correct every week is the winner!
-        Ties are broken by also predicting the total number of points scored in
-        the Pittsburgh Steeler's game that week.
+        While what I was able to do with OpenCV in the past was cool, I really wanted people to be
+        able to experience what was possible with the library without the pain of setting up a
+        developer environment or having to install a standalone binary. Surely, there must be a version
+        of this library written in JavaScript, right?
       </template>
     </StoryPoint>
-    <StoryPoint :wide="wide" icon="redeem" :left="true">
-      <template #label> What is the prize? </template>
+    <StoryPoint :wide="wide" icon="event_available" :left="true">
+      <template #label> Easy enough... </template>
       <template #content>
-        Bragging rights. However, bragging rights are important sometimes.
-        Recently, I had an idea about how to improve my performance in the
-        Football Pool...
+        The basic overview of the process to use OpenCV on the web is simple. Download the
+        pre-compiled JavaScript file from their
+        <a href="https://docs.opencv.org/4.6.0/d0/d84/tutorial_js_usage.html" target="_blank">website</a>
+        and create a <code>script</code> tag reffering to the file to use it in other code. Better yet, there
+        are also pre-compiled versions of OpenCV available as
+        <a href="https://www.npmjs.com/package/@techstark/opencv-js" target="_blank">NPM modules</a>. The developer
+        experience is made even smoother with the <code>mirada</code>&nbsp;
+        <a href="https://www.npmjs.com/package/mirada" target="_blank">typing module</a>. This should allow me to easily
+        set up some basic computer vision and 3D reconstruction examples.
       </template>
     </StoryPoint>
-    <StoryPoint :wide="wide" icon="online_prediction">
+    <StoryPoint :wide="wide" icon="event_busy">
       <template #label>
-        An idea is born<a
-          href="https://www.youtube.com/watch?v=Ax7QSsP9YDo&ab_channel=p1nk"
-          target="_blank"
-          style="text-decoration: none"
-          >!</a
-        >
+        Or so I thought
       </template>
       <template #content>
-        What if I could build a system that predicts the outcome of a football
-        game? What about a system that could grade my picks after each week?
-        Maybe such a system could help me improve picks that I am only
-        moderately confident in.
+        Setting up a scene and some cameras with <code><a href="https://threejs.org/" target="_blank">Three.js</a></code>
+        was fairly easy, as was setting up a stereo vision system in that scene. Effeciently moving the data from a given
+        stereo vision frame into an OpenCV <code>Mat</code> fell into place not long after. However, I came across a problem
+        when trying to use some of the 3D calibration and reconstruction functions. They were <b>not there</b>!
       </template>
     </StoryPoint>
     <StoryPoint :wide="wide" icon="data_thresholding" :left="true">
@@ -85,10 +80,8 @@
         <b>at a certain point</b>
         during that game would be useful to me. In that case, I can tell the
         system,
-        <i
-          >"Given an ordered list of previous plays in a game, predict the
-          winner of this game"</i
-        >.
+        <i>"Given an ordered list of previous plays in a game, predict the
+          winner of this game"</i>.
       </template>
     </StoryPoint>
     <StoryPoint :wide="wide" icon="question_mark" :left="true">
@@ -97,8 +90,7 @@
         While the system I have described above might not tell me exactly who
         will win ahead of time, it could still grade teams week to week on how
         realiable they are relative to the points
-        <span class="text-info"
-          >spread.
+        <span class="text-info">spread.
           <q-tooltip class="text-body2" max-width="25vw">
             The spread is the number of points statisticians beleive will be the
             difference between the final scores
@@ -110,13 +102,11 @@
     </StoryPoint>
     <q-item class="q-pt-xl">
       <q-item-section class="col-12">
-        <q-item-label
-          :class="{
-            'text-h3': wide,
-            'text-h4': !wide,
-            'text-weight-light text-center': true,
-          }"
-        >
+        <q-item-label :class="{
+          'text-h3': wide,
+          'text-h4': !wide,
+          'text-weight-light text-center': true,
+        }">
           Lets dive into the design of this system
         </q-item-label>
       </q-item-section>
