5976ea6ebb91dc48a2eb54fbac3c9ec6bb63607f
DESKTOP-QKIA14E\danny <dallman@appliedres.com>
2023-03-26 01:06:57 -0700

    adding an actual rnn from the book

--- portfolio/src/components/TFCont.vue
+++ portfolio/src/components/TFCont.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script>
-import codes from '../js/workerCodes.json'
+import codes from '../js/workerCodes.json';
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
+import ds from 'src/assets/processed_games/fullDS.json';
 export default {
   setup() {
     console.log(process);
@@ -18,77 +21,192 @@ export default {
     });
 
     w.onmessage = function (event) {
-      switch(event.data[0]){
+      switch (event.data[0]) {
         case codes.TENSOR_LOADED: {
-          w.postMessage([codes.GET_SCATTERPLOT_DATA])
+          w.postMessage([codes.GET_SCATTERPLOT_DATA]);
+          break;
         }
         case codes.SCATTERPLOT_DATA_RESP: {
-          console.log(event)
+          /** @type {Int32Array} */
+          let awayBuff = event.data[1];
+          /** @type {Int32Array} */
+          let homeBuff = event.data[2];
+
+          let awayScatter = Array.from(awayBuff)
+            .filter((el, idx) => (idx + 1) % 204 != 0)
+            .map((el, idx) => ({ x: idx % 203, y: el }));
+          let homeScatter = Array.from(homeBuff)
+            .filter((el, idx) => (idx + 1) % 204 != 0)
+            .map((el, idx) => ({ x: idx % 203, y: el }));
+
+          let a = tf.tensor(awayBuff, [awayBuff.length / 204, 204, 1], 'int32');
+          let h = tf.tensor(homeBuff, [homeBuff.length / 204, 204, 1], 'int32');
+          console.log(awayScatter);
+          console.log(homeScatter);
+
+          tfvis.show.valuesDistribution(
+            { name: 'quarter vals dist', tab: 'away' },
+            a
+          );
+          tfvis.show.valuesDistribution(
+            { name: 'quarter vals dist', tab: 'home' },
+            h
+          );
+          tfvis.render.heatmap(
+            { name: 'quarter heatmap', tab: 'away' },
+            { values: a.arraySync() }
+          );
+          tfvis.render.heatmap(
+            { name: 'quarter heatmap', tab: 'home' },
+            { values: h.arraySync() }
+          );
+
+          tfvis.render.scatterplot(
+            { name: 'quarter vals scatter', tab: 'home' },
+            { values: [homeScatter], series: ['home'] }
+          );
+          tfvis.render.scatterplot(
+            { name: 'quarter vals scatter', tab: 'away' },
+            { values: [awayScatter], series: ['away'] }
+          );
+          break;
         }
       }
     };
     console.log(w);
-    w.postMessage([codes.LOAD_DATA]);
+    // w.postMessage([codes.LOAD_DATA]);
 
-    // let csvFile = tf.data.csv(
-    //   'src/assets/processed_games/away/201209050nyg.csv',
-    //   {
-    //     hasHeader: false,
-    //     columnNames: [
-    //       'quarter',
-    //       'timeLeft',
-    //       'down',
-    //       'distance',
-    //       'oppTerritory',
-    //       'yard',
-    //       'homeScore',
-    //       'awayScore',
-    //     ],
-    //   }
-    // );
+    function createData() {
+      /** @type {Array} */
+      const data = ds.data;
+      let dataMap = {
+        games: [],
+        labels: [],
+        breakpoint: 0,
+      };
 
-    // const mapped = csvFile
-    //   .map((el) => {
-    //     return { xs: Object.values(el), ys: [1] };
-    //   })
-    //   .batch(10);
+      let finalSum = 0;
+      data.forEach(
+        /** @param {Array} game */ (game) => {
+          finalSum = game.at(-1).reduce((acc, el) => acc + el, 0);
+          if (finalSum) {
+            dataMap.games.push(
+              game
+                .slice(0, -1)
+                .map(/** @param {Array} play */ (play) => play.slice(0, 6)) // this removes scores
+            );
+            dataMap.labels.push(true);
+          } else {
+            dataMap.games.push(
+              game
+                .slice(0, -1)
+                .map(/** @param {Array} play */ (play) => play.slice(0, 6)) // this removes scores
+            );
+            dataMap.labels.push(false);
+          }
+        }
+      );
 
-    // csvFile.columnNames().then((colNames) => {
-    //   const model = tf.sequential();
-    //   model.add(
-    //     tf.layers.dense({
-    //       units: 20,
-    //       activation: 'sigmoid',
-    //       inputShape: [8],
-    //     })
-    //   );
-    //   model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
-    //   model.compile({
-    //     loss: 'binaryCrossentropy',
-    //     optimizer: tf.train.sgd(0.000001),
-    //     metrics: ['accuracy'],
-    //   });
+      tf.util.shuffleCombo(dataMap.games, dataMap.labels);
 
-    //   const { container, label, drawArea } = tfvis
-    //     .visor()
-    //     .surface({ name: 'fitCallbacks', tab: 'training' });
+      const trainBreak = Math.floor(dataMap.games.length * 0.7);
+      const testBreak = Math.floor(dataMap.games.length * 0.9);
 
-    //   model
-    //     .fitDataset(mapped, {
-    //       epochs: 30,
-    //       callbacks: tfvis.show.fitCallbacks(container, ['loss', 'acc']),
-    //     })
-    //     .then((res) => {
-    //       console.log(res);
-    //       mapped.forEachAsync((el) => {
-    //         const res = model.predictOnBatch(el.xs);
-    //         res.print();
-    //       });
-    //     });
-    // });
+      return {
+        train: {
+          x: tf.tensor(
+            dataMap.games.slice(0, trainBreak),
+            [trainBreak, 203, 6],
+            'int32'
+          ),
+          y: tf.tensor(
+            dataMap.labels.slice(0, trainBreak),
+            [trainBreak, 1],
+            'bool'
+          ),
+        },
+        test: {
+          x: tf.tensor(
+            dataMap.games.slice(trainBreak, testBreak),
+            [testBreak - trainBreak, 203, 6],
+            'int32'
+          ),
+          y: tf.tensor(
+            dataMap.labels.slice(trainBreak, testBreak),
+            [testBreak - trainBreak, 1],
+            'bool'
+          ),
+        },
+        val: {
+          x: tf.tensor(
+            dataMap.games.slice(testBreak),
+            [dataMap.games.length - testBreak, 203, 6],
+            'int32'
+          ),
+          y: tf.tensor(
+            dataMap.labels.slice(testBreak),
+            [dataMap.games.length - testBreak, 1],
+            'bool'
+          ),
+        },
+      };
+    }
 
-      // as of now, this trains a model on one single game file. it then predicts the data from that game
-      //model is probably also bs but we have succesfully read in data and trained with it
+    function createRnn(units, inputShape) {
+      const model = tf.sequential();
+      model.add(
+        tf.layers.simpleRNN({
+          units,
+          inputShape,
+          inputDType: 'int32'
+        })
+      );
+      model.add(
+        tf.layers.dense({
+          units: 1,
+          activation: 'sigmoid',
+        })
+      );
+      return model;
+    }
+
+    async function trainNetwork() {
+      const units = 40,
+        inputShape = [203, 6];
+      const model = createRnn(units, inputShape);
+      const data = createData();
+
+      model.compile({
+        loss: 'binaryCrossentropy',
+        optimizer: 'adam',
+        metrics: ['acc'],
+      });
+      model.summary();
+      // data.train.x.print();
+      // data.train.y.print();
+      // data.test.x.print();
+      // data.test.y.print();
+      // data.val.x.print();
+      // data.val.y.print();
+
+      console.log('fitting');
+      await model.fit(data.train.x, data.train.y, {
+        epochs: 5,
+        validationData: [data.test.x, data.test.y],
+      });
+      console.log('eval');
+      const [testLoss, testAcc] = model.evaluate(data.val.x, data.val.y);
+      console.log(`Evaluation loss: ${(await testLoss.data())[0].toFixed(4)}`);
+      console.log(
+        `Evaluation accuracy: ${(await testAcc.data())[0].toFixed(4)}`
+      );
+    }
+
+    trainNetwork();
+    // as of now, this trains a model on one single game file. it then predicts the data from that game
+    //model is probably also bs but we have succesfully read in data and trained with it
+
+    //============================================================================================
   },
 };
 </script>
--- portfolio/src/js/workerTest.js
+++ portfolio/src/js/workerTest.js
@@ -18,33 +18,60 @@ import codes from './workerCodes.json';
  */
 
 let godTensor;
+let labelMap = {
+  games: [],
+  labels: [],
+  breakpoint: 0,
+};
 let slices = {
-  /** @type {tf.Tensor} */
-  quarter: {},
-  /** @type {tf.Tensor} */
-  time: {},
-  /** @type {tf.Tensor} */
-  down: {},
-  /** @type {tf.Tensor} */
-  distance: {},
-  /** @type {tf.Tensor} */
-  yard: {},
-  /** @type {tf.Tensor} */
-  opp: {},
-  /** @type {tf.Tensor} */
-  home: {},
-  /** @type {tf.Tensor} */
-  away: {},
+  home: {
+    /** @type {tf.Tensor} */
+    quarter: {},
+    /** @type {tf.Tensor} */
+    time: {},
+    /** @type {tf.Tensor} */
+    down: {},
+    /** @type {tf.Tensor} */
+    distance: {},
+    /** @type {tf.Tensor} */
+    yard: {},
+    /** @type {tf.Tensor} */
+    opp: {},
+    /** @type {tf.Tensor} */
+    home: {},
+    /** @type {tf.Tensor} */
+    away: {},
+  },
+  away: {
+    /** @type {tf.Tensor} */
+    quarter: {},
+    /** @type {tf.Tensor} */
+    time: {},
+    /** @type {tf.Tensor} */
+    down: {},
+    /** @type {tf.Tensor} */
+    distance: {},
+    /** @type {tf.Tensor} */
+    yard: {},
+    /** @type {tf.Tensor} */
+    opp: {},
+    /** @type {tf.Tensor} */
+    home: {},
+    /** @type {tf.Tensor} */
+    away: {},
+  },
 };
 
 onmessage = function (event) {
   switch (event.data[0]) {
     case codes.LOAD_DATA: {
-      createTensor();
+      // createTensor();
+      trainNetwork();
       break;
     }
     case codes.GET_SCATTERPLOT_DATA: {
-      postScatterplotData();
+      // postScatterplotData();
+
       break;
     }
   }
@@ -54,13 +81,43 @@ onmessage = function (event) {
 const filterGameToOneFeature = (game, idx) => game.map((play) => [play[idx]]);
 
 function createTensor() {
+  let ten2d = tf.tensor3d(ds.data, [ds.data.length, 204, 8], 'int32');
+  ten2d.print();
+
   godTensor = tf.tensor(ds.data, [ds.data.length, 204, 8], 'int32');
   godTensor.print();
 
-  Object.entries(slices).forEach((entry, idx) => {
-    slices[entry[0]] = tf.tensor(
-      ds.data.map((game) => filterGameToOneFeature(game, idx)),
-      [ds.data.length, 204, 1],
+  let finalSum = 0;
+  ds.data.forEach((game) => {
+    finalSum = game.at(-1).reduce((acc, el) => acc + el, 0);
+    if (finalSum) {
+      labelMap.games.push(game);
+      labelMap.labels.push(1);
+    } else {
+      labelMap.games.push(game);
+      labelMap.labels.push(0);
+    }
+  });
+
+  labelMap.breakpoint = labelMap.labels.indexOf(1);
+
+  console.log(labelMap);
+
+  Object.entries(slices.away).forEach((entry, idx) => {
+    slices.away[entry[0]] = tf.tensor(
+      ds.data
+        .slice(0, labelMap.breakpoint)
+        .map((game) => filterGameToOneFeature(game, idx)),
+      [labelMap.breakpoint, 204, 1],
+      'int32'
+    );
+  });
+  Object.entries(slices.home).forEach((entry, idx) => {
+    slices.home[entry[0]] = tf.tensor(
+      ds.data
+        .slice(labelMap.breakpoint)
+        .map((game) => filterGameToOneFeature(game, idx)),
+      [ds.data.length - labelMap.breakpoint, 204, 1],
       'int32'
     );
   });
@@ -70,6 +127,135 @@ function createTensor() {
 }
 
 function postScatterplotData() {
+  postMessage([
+    codes.SCATTERPLOT_DATA_RESP,
+    slices.away.quarter.bufferSync().values,
+    slices.home.quarter.bufferSync().values,
+  ]);
+}
 
-  postMessage([codes.SCATTERPLOT_DATA_RESP, slices.quarter.bufferSync().values])
+//============================================================================================
+
+function createData() {
+  /** @type {Array} */
+  const data = ds.data;
+  let dataMap = {
+    games: [],
+    labels: [],
+    breakpoint: 0,
+  };
+
+  let finalSum = 0;
+  data.forEach(
+    /** @param {Array} game */ (game) => {
+      finalSum = game.at(-1).reduce((acc, el) => acc + el, 0);
+      if (finalSum) {
+        dataMap.games.push(
+          game
+            .slice(0, -1)
+            .map(/** @param {Array} play */ (play) => play.slice(0, 6)) // this removes scores
+        );
+        dataMap.labels.push(true);
+      } else {
+        dataMap.games.push(
+          game
+            .slice(0, -1)
+            .map(/** @param {Array} play */ (play) => play.slice(0, 6)) // this removes scores
+        );
+        dataMap.labels.push(false);
+      }
+    }
+  );
+
+  tf.util.shuffleCombo(dataMap.games, dataMap.labels);
+
+  const trainBreak = Math.floor(dataMap.games.length * 0.7);
+  const testBreak = Math.floor(dataMap.games.length * 0.9);
+
+  return {
+    train: {
+      x: tf.tensor(
+        dataMap.games.slice(0, trainBreak),
+        [trainBreak, 203, 6],
+        'int32'
+      ),
+      y: tf.tensor(
+        dataMap.labels.slice(0, trainBreak),
+        [trainBreak, 1],
+        'bool'
+      ),
+    },
+    test: {
+      x: tf.tensor(
+        dataMap.games.slice(trainBreak, testBreak),
+        [testBreak - trainBreak, 203, 6],
+        'int32'
+      ),
+      y: tf.tensor(
+        dataMap.labels.slice(trainBreak, testBreak),
+        [testBreak - trainBreak, 1],
+        'bool'
+      ),
+    },
+    val: {
+      x: tf.tensor(
+        dataMap.games.slice(testBreak),
+        [dataMap.games.length - testBreak, 203, 6],
+        'int32'
+      ),
+      y: tf.tensor(
+        dataMap.labels.slice(testBreak),
+        [dataMap.games.length - testBreak, 1],
+        'bool'
+      ),
+    },
+  };
+}
+
+function createRnn(units, inputShape) {
+  const model = tf.sequential();
+  model.add(
+    tf.layers.simpleRNN({
+      units,
+      inputShape,
+      // inputDType: 'int32'
+    })
+  );
+  model.add(
+    tf.layers.dense({
+      units: 1,
+      activation: 'sigmoid',
+    })
+  );
+  return model;
+}
+
+async function trainNetwork() {
+  const units = 40,
+    inputShape = [203, 6];
+  const model = createRnn(units, inputShape);
+  const data = createData();
+
+  model.compile({
+    loss: 'binaryCrossentropy',
+    optimizer: 'adam',
+    metrics: ['acc'],
+  });
+  model.summary();
+  data.train.x.print();
+  data.train.y.print();
+  data.test.x.print();
+  data.test.y.print();
+  data.val.x.print();
+  data.val.y.print();
+
+  console.log('fitting')
+  await model.fit(data.train.x, data.train.y, {
+    epochs: 1,
+    validationData: [data.val.x, data.val.y]
+  });
+  console.log('eval')
+  const [testLoss, testAcc] = model.evaluate(data.test.x, data.test.y);
+  console.log(`Evaluation loss: ${(await testLoss.data())[0].toFixed(4)}`);
+  console.log(`Evaluation accuracy: ${(await testAcc.data())[0].toFixed(4)}`);
 }
